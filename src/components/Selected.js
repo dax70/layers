70610129111e288369d7315027a0a1f874b57044
Eddy Recio <eddy@ngensoft.net>
2017-04-23 00:23:44 -0700

    Selectable initial styling - Minor refactor of test cases.

--- src/components/Selected.js
+++ src/components/Selected.js
@@ -1,24 +1,85 @@
 import React from 'react';
 import { css } from 'glamor';
-import glamorous from 'glamorous';
-import Shape from './Shape';
+import Shape, { computeStylesFromProps }  from './Shape';
+
+const handleStyle = css({
+  backgroundColor: '#fff',
+  border: '1px solid #bbb',
+  boxShadow: '0px 1px 1px 0px rgba(0, 0, 0, 0.2)'
+});
+
+const border = css({
+  border: '1px solid #bbb',
+  margin: '3px',
+  width: 'calc(100% - 7px)',
+  height: 'calc(100% - 7px)'
+});
+
+const topLeft = css({
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  width: 9,
+  height: 9
+});
+
+const topRight = css({
+  position: 'absolute',
+  right: 0,
+  top: 0,
+  width: 9,
+  height: 9
+});
+
+const bottomLeft = css({
+  position: 'absolute',
+  left: 0,
+  bottom: 0,
+  width: 9,
+  height: 9
+});
+
+const bottomRight = css({
+  position: 'absolute',
+  right: 0,
+  bottom: 0,
+  width: 9,
+  height: 9
+});
 
 export default class Selectaed extends Shape {
 
+  computeStyles() {
+    const prevProps = this.props;
+    const newProps = {
+      x: prevProps.x -4,
+      y: prevProps.y -4,
+      width: prevProps.width +9,
+      height: prevProps.height +9
+    };
+    const props = Object.assign({}, prevProps, newProps);
+    const baseCss = computeStylesFromProps(props);
+    const selected = css({
+      fontWeight: 'bolder'
+    });
+
+    return css(baseCss, selected);
+  }
+
   renderChildren() {
 
+
     return (
-      <div>
-        foo
-        <div className="border">
+      <div style={{ width: '100%', height: '100%'}}>
+        <div {...border}>
         </div>
-        <div className="top-left handle">
+        <div {...css(topLeft, handleStyle)}>
         </div>
-        <div className="top-right handle">
+        <div {...css(topRight, handleStyle)}>
         </div>
-        <div className="bottom-left handle">
+        <div {...css(bottomLeft, handleStyle)}>
         </div>
-        <div className="bottom-right handle">
+        <div {...css(bottomRight, handleStyle)}>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,33 +4,30 @@ import App from './App';
 import './index.css';
 import { Document, Layer, Shape } from './models';
 
-const yellowShape = new Shape({
-  color: 'yellow',
+const blueishShape = new Shape({
+  color: '#4A90E2',
   x: 100,
   y:100,
   width: 50,
   height: 50
 });
 
-const blueShape = new Shape({
-  color: 'blue',
-  x: 150,
-  y: 150,
+const greenishShape = new Shape({
+  color: '#B8E986',
+  x: 250,
+  y: 75,
   width: 100,
   height: 100
 });
 
 const layer = new Layer();
-layer.addShape(yellowShape);
-layer.addShape(blueShape);
+layer.addShape(blueishShape);
+layer.addShape(greenishShape);
 
 const doc = new Document();
 doc.add(layer);
-// doc.add(yellowShape);
-// doc.add(blueShape);
-
 
 ReactDOM.render(
-  <App  document={doc}/>,
+  <App document={doc}/>,
   document.getElementById('root')
 );
